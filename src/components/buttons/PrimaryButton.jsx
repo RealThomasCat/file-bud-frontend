fb9062f4059bc71276707ffb623d76f3899aba8b
RealThomasCat <yashveer.rtc@gmail.com>
2024-08-02 15:24:17 +0530

    Fixed landing page UI for mobile screen size.

--- src/components/buttons/PrimaryButton.jsx
+++ src/components/buttons/PrimaryButton.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 
-function PrimaryButton({ title, action, v2 }) {
+function PrimaryButton({ title, action, v2, width }) {
     return (
         <button
             onClick={action}
-            className={`h-full ${v2 ? "w-full" : "w-20 md:w-44"} rounded-full bg-primary hover:tracking-wider duration-500 text-xs md:text-lg font-medium text-textCol md:pb-[1px] uppercase`}
+            className={`h-full ${v2 ? "w-full" : width ? `w-${width} md:w-44` : "w-20 md:w-44"} rounded-full bg-primary hover:tracking-wider duration-500 text-xs md:text-lg font-medium text-textCol md:pb-[1px] uppercase`}
         >
             {title}
         </button>
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -8,8 +8,8 @@ function LandingPage() {
         <Container>
             <div className="flex justify-between items-center my-20">
                 {/* Hero Content */}
-                <div className="h-128 flex flex-col w-1/2 gap-8">
-                    <h1 className="text-7xl uppercase font-bold text-textCol ">
+                <div className="h-128 flex flex-col w-full md:w-1/2 gap-8">
+                    <h1 className="text-4xl md:text-7xl uppercase font-bold text-textCol ">
                         <span className="">Powerful</span>
                         <br />
                         Storage For
@@ -17,19 +17,22 @@ function LandingPage() {
                         <span className="">Your Files</span>
                     </h1>
 
-                    <div className="flex gap-14 text-2xl uppercase font-medium text-primary">
+                    <div className="flex md:flex-row gap-6 md:gap-14 text-lg md:text-2xl uppercase font-medium text-primary">
                         <p className="pl-1">Store</p>
                         <p className="pl-1">Stream</p>
                         <p className="pl-1">Download</p>
                     </div>
 
-                    <Link to="/home" className="h-12 rounded-full w-fit mt-3">
-                        <PrimaryButton title="Start Using" />
+                    <Link
+                        to="/home"
+                        className="h-12 rounded-full md:w-fit mt-3"
+                    >
+                        <PrimaryButton title="Start Using" width="28" />
                     </Link>
                 </div>
 
                 {/* Hero Image */}
-                <div className="h-full flex justify-center items-center">
+                <div className="h-full hidden md:flex justify-center items-center">
                     <img
                         className="h-96 object-contain pt-4 pb-2"
                         src={heroImage}
